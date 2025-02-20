7ada87191b01f8d02702471b4b92f31c6f40b73d
BhanuGangu <bhanugangu2000@gmail.com>
2025-02-20 11:38:25 -0500

    Update create_bucket.py

--- lab3/create_bucket.py
+++ lab3/create_bucket.py
@@ -1,1 +1,7 @@
+import boto3
 
+s3 = boto3.client('s3')
+bucket_name = 'bucketlab3cbhanu'  # Change this to a globally unique name
+
+response = s3.create_bucket(Bucket=bucket_name)
+print(f'Bucket {bucket_name} created successfully!')
